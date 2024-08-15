7ee471353959ae8a87102ebe78d144c1161e8eea
lobarashurova <106048869+lobarashurova@users.noreply.github.com>
2024-08-15 16:06:46 +0500

    finished

--- lib/presentation/screens/add_order/bloc/add_order_bloc.dart
+++ lib/presentation/screens/add_order/bloc/add_order_bloc.dart
@@ -9,20 +9,24 @@ part 'add_order_state.dart';
 class AddOrderBloc extends Bloc<AddOrderEvent, AddOrderState> {
   AddOrderBloc() : super(AddOrderState()) {
     int totalCost = 0;
+    String type = "";
     final repo = getIt<AppRepository>();
     on<AddOrder>((event, emit) async {
       repo.addProduct(event.productModel);
+
       emit(state.copyWith(
-          products: (await repo.getAllProducts())
-              .where((item) => item.productType == event.type)
-              .toList(),
-          totalCost: (state.totalCost ?? 0) + 12));
+        products: (await repo.getAllProducts())
+            .where((item) => item.productType == event.type)
+            .toList(),
+        totalCost: (state.totalCost ?? 0) + 12,
+      ));
     });
 
     on<ChangeMenu>((event, emit) async {
       emit(state.copyWith(isMenu: event.isMenu));
     });
     on<GetOrdersByType>((event, emit) async {
+      type = event.type;
       List<ProductModel> prodcuts = (await repo.getAllProducts())
           .where((item) => item.productType == event.type)
           .toList();
@@ -32,14 +36,19 @@ class AddOrderBloc extends Bloc<AddOrderEvent, AddOrderState> {
       emit(state.copyWith(products: prodcuts, totalCost: totalCost.toDouble()));
     });
     on<UpdateProductModel>((event, emit) async {
+      print("${event.productModel.cost}     ${event.cost} ");
       emit(state.copyWith(
           totalCost:
-              totalCost.toDouble() - event.productModel.cost + event.cost));
+              (state.totalCost ?? 0) - event.productModel.cost + event.cost));
 
       await repo.updateProduct(
         event.cost,
         event.id,
       );
+      emit(state.copyWith(
+          products: (await repo.getAllProducts())
+              .where((item) => item.productType == type)
+              .toList()));
     });
   }
 }
